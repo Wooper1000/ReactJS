b7ef2682305ccc7f874b407683899e1750940a14
Wooper1000 <woper1000@gmail.com>
2021-08-23 21:55:21 +0300

    dialogs logic added

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,7 +1,34 @@
-const Dialogs = ()=>{
-    return(
-        <div>
-            Здарова
+import s from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
+
+const Dialog = (props) => {
+    let path = '/dialogs/'
+    return (
+        <div className={s.dialog}>
+            <NavLink to={ path + props.Id}>{props.name}</NavLink>
+        </div>
+    )
+}
+const Message = (props)=>{
+    return (
+        <div className={s.message}>{props.message}</div>
+    )
+}
+const Dialogs = () => {
+    return (
+        <div className={s.dialogs}>
+            <div className={s.dialogItems}>
+                <Dialog name="Даня" Id="1"/>
+                <Dialog name="Маша" Id="2"/>
+                <Dialog name="Рома" Id="3"/>
+                <Dialog name="Макс" Id="4"/>
+
+            </div>
+            <div className={s.messages}>
+                <Message message={'Хэллоу'}/>
+                <Message message={'чо молчишь'}/>
+                <Message message={'Бират'}/>
+            </div>
         </div>
     )
 }
